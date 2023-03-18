d1b8c6d334ea4cb275af462989f9d23bc3e98d42
thefutureseer <danielharrislimos@outlook.com>
2023-03-18 15:35:59 -0700

    fix prop key

--- pages/api/About.js
+++ pages/api/About.js
@@ -1,30 +1,30 @@
-//Riligions will be populated at build time 
-function Blog({ posts }) {
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li>{post.title}</li>
-      ))}
-    </ul>
-  )
-}
+// //Riligions will be populated at build time 
+// function Blog({ posts }) {
+//   return (
+//     <ul>
+//       {posts.map((post) => (
+//         <li>{post.title}</li>
+//       ))}
+//     </ul>
+//   )
+// }
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side
-//can do direct database queries.
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const res = await fetch('https://.../posts')
-  const posts = await res.json()
+// // This function gets called at build time on server-side.
+// // It won't be called on client-side
+// //can do direct database queries.
+// export async function getStaticProps() {
+//   // Call an external API endpoint to get posts.
+//   // You can use any data fetching library
+//   const res = await fetch('https://.../posts')
+//   const posts = await res.json()
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      posts,
-    },
-  }
-}
+//   // By returning { props: { posts } }, the Blog component
+//   // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       posts,
+//     },
+//   }
+// }
 
-export default Blog
+// export default Blog
--- pages/api/MessageBoard.js
+++ pages/api/MessageBoard.js
@@ -1,11 +1,11 @@
-export default function MessageBoard({data}) {
-  return <div>Heloo spirit</div>
-};
+// export default function MessageBoard({data}) {
+//   return <div>Heloo spirit</div>
+// };
 
-export async function getServerSideProps() {
-  //serch data from external api
-  const res = await fetch("https://localhost:5000");
-  const data = await res.json();
-  //pass data to the page via props
-  return {props: {data}}
-};
+// export async function getServerSideProps() {
+//   //serch data from external api
+//   const res = await fetch("https://localhost:5000");
+//   const data = await res.json();
+//   //pass data to the page via props
+//   return {props: {data}}
+// };
